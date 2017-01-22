086527ce11495105120a1c11cf6a8790a7a90d79
Hung Le <hungle@Hungs-MacBook-Pro.local>
2017-01-22 10:13:05 -0700

    Adding comment in app.js

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -7,6 +7,7 @@
     LunchCheckController.$inject = ['$scope'];
 
     function LunchCheckController($scope) {
+        // Define the text for result statement
         var stmt = {
             NODATA: "Please Enter Data First!",
             ENJOY : "Enjoy!",
@@ -16,17 +17,21 @@
         $scope.result_statement = "";
 
         $scope.checkFoodItems = function () {
+            // Flag empty entry
             if ($scope.food.length === 0) {
                 $scope.result_statement = stmt.NODATA;
                 return;
             }
+            // Split entry into an array
             var foodItems = $scope.food.split(",");
             var actualCnt = 0;
+            // Remove empty items (empty spaces)
             for (var i = 0; i < foodItems.length; i++) {
                 if (foodItems[i] !== "") {
                     actualCnt += 1;
                 }
             }
+            // Compare result and issue statement
             if (actualCnt <= 3) {
                 $scope.result_statement = stmt.ENJOY;
             } else {
